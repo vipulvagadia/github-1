199565c2af70e5fd497d7ce46c63db217cf8681e
ZeronSec <ZeronSec@DESKTOP-BCF229R>
2021-03-27 15:03:24 +0530

    comit git

---
+++ githab-1/src/main/java/com/git/Githab1Application.java
@@ -0,0 +1,14 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Githab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Githab1Application.class, args);
+		System.out.println("git hub");
+	}
+
+}
---
+++ githab-1/src/test/java/com/git/Githab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Githab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
